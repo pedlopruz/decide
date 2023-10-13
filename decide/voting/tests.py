2f833fcd813ccf72c4128b7a0a178c5d0b026616
pedlopruz <plopezr2011@gmail.com>
2023-10-13 11:41:56 +0200

    feat: Nuevos test de prueba
    Añadidos test de votación

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -24,13 +24,27 @@ from voting.models import Voting, Question, QuestionOption
 from datetime import datetime
 
 
-class VotingTestCase(BaseTestCase):
-
+class VotingModelTestCase(BaseTestCase):
     def setUp(self):
+        q = Question(desc='Descripcion')
+        q.save()
+        
+        opt1 = QuestionOption(question=q, option='opcion 1')
+        opt1.save()
+        opt1 = QuestionOption(question=q, option='opcion 2')
+        opt1.save()
+
+        self.v = Voting(name='Votacion', question=q)
+        self.v.save()
         super().setUp()
 
     def tearDown(self):
         super().tearDown()
+        self.v = None
+
+    def testExist(self):
+        v=Voting.objects.get(name='Votacion')
+        self.assertEquals(v.question.options.all()[0].option, "opcion 1")
 
     def encrypt_msg(self, msg, v, bits=settings.KEYBITS):
         pk = v.pub_key
